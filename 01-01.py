368998afb1fc2bdb4b5c98eaf3d0c19da936d497
LS68048 <p68048@lutonsfc.ac.uk>
2024-09-12 08:37:15 +0100

    Complete 01-01

---
+++ 01-01.py
@@ -0,0 +1,13 @@
+# Hello World program
+
+# -------------------------
+# Subprograms
+# -------------------------
+def output():
+    print("Hello World")
+    print("This is my first program")
+    
+# -------------------------
+# Main program
+# -------------------------
+output()
