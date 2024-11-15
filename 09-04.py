0870df33364b13496e36afcf048032d95f3dd35f
Alex Wells <p68048@lutonsfc.ac.uk>
2024-11-15 14:41:47 +0000

    Complete 09-04

---
+++ 09-04.py
@@ -0,0 +1,24 @@
+# Quote of the day program
+
+# -------------------------
+# Import libraries
+# -------------------------
+import random
+
+# -------------------------
+# Subprograms
+# -------------------------
+def random_quote():
+    quotes = [["Any fool can write code that a computer can understand. Good programmers write code that humans can understand.", "Martin Fowler"],
+    ["Code is like humour. When you have to explain it, it’s bad.", "Cory House"],
+    ["Simplicity is the soul of efficiency.", "Austin Freeman"]]
+
+    return quotes[random.randint(0,2)]
+
+
+# -------------------------
+# Main program
+# -------------------------
+quote = random_quote()
+print(quote[0])
+print(quote[1])
